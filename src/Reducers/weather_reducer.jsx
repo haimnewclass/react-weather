e2581633c68a8b6aff037f9906487c7e94dac37c
nitc310 <nitc310@classes.nitc-students.co.il>
2022-03-21 14:31:20 +0200

    Add Redux

--- src/Reducers/weather_reducer.jsx
+++ src/Reducers/weather_reducer.jsx
@@ -3,19 +3,29 @@ export const SearchAction=(data)=>{
     return {type:'WEATHER.SEARCH',
             data:data};
 }
+export const addToHistoryAction=(data)=>{
+    return {type:'WEATHER.ADD-TO-HISTORY',
+            data:data};
+}
 export const clearAction=()=>{
     return {type:'WEATHER.CLEAR'};
 }
 
 // Reducer
-export const reducerWeather=(state={},action)=>{
+//export const reducerWeather=(state={lastSearthResult:'',history:[]},action)=>{
+    export const reducerWeather=(state={},action)=>{
+
+    
 
     switch(action.type)
     {
         case "WEATHER.SEARCH":
         return state=action.data;
         break;
-        case "WEATHER.CLEAR":
+        case "WEATHER.ADD-TO-HISTORY":
+            return state=action.data;
+        break;
+            case "WEATHER.CLEAR":
         return state={};
         break;
         
